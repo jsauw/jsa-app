26b38fc68999d4a29ca12feb71345cc2eafcf196
rioishii <rioishii@uw.edu>
2018-12-05 19:22:24 -0800

    Mission

--- src/Homepage.js
+++ src/Homepage.js
@@ -7,6 +7,7 @@ class Homepage extends Component {
         return (
             <div>
                 <Header />
+                <Mission />
             </div>
         )
     }
@@ -24,6 +25,28 @@ class Header extends Component {
                     </div>
                 </div>
             </header>
+        )
+    }
+}
+
+class Mission extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <img className="img-fluid" src="./imgs/mission.jpg" alt="" />
+                    </div>
+
+                    <div className="col-lg-6">
+                        <h3 className="my-3">Our Mission</h3>
+                        <p>Our goal is to spread the Japanese culture to students in a fun and informative environment. We offer many cultural events and social gatherings throughout the year.  </p>
+                        <br></br>
+                        <p>We encourage all students to join JSA as we are a very open and diverse group. This association is driven by the energy of its members, and we welcome their ideas and comments to further make this club more enjoyable. We have a strong executive board more than willing to listen to the input of its members and to maximize their enjoyment and participation with JSA. </p>
+                    </div>
+
+                </div>
+            </div>
         )
     }
 }
