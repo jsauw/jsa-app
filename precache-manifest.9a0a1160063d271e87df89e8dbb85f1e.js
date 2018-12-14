d901e36831f98ab1b3896e5540b64b41bd4919fb
rioishii <rioishii@uw.edu>
2018-12-14 11:07:38 -0800

    Updates

--- precache-manifest.9a0a1160063d271e87df89e8dbb85f1e.js
+++ precache-manifest.9a0a1160063d271e87df89e8dbb85f1e.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/jsa-app/static/media/fontawesome-webfont.674f50d2.eot"
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/jsa-app/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "f82114adc9568f8d3309",
+    "revision": "697b4985bba63789d1bd",
     "url": "/jsa-app/static/css/main.395b3f50.chunk.css"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/jsa-app/static/media/fontawesome-webfont.af7ae505.woff2"
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/jsa-app/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "b4d13a0496a6960dfd55",
@@ -24,12 +24,8 @@ self.__precacheManifest = [
     "url": "/jsa-app/static/media/masthead.60d21051.jpg"
   },
   {
-    "revision": "f82114adc9568f8d3309",
-    "url": "/jsa-app/static/js/main.f82114ad.chunk.js"
-  },
-  {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/jsa-app/static/media/fontawesome-webfont.fee66e71.woff"
+    "revision": "697b4985bba63789d1bd",
+    "url": "/jsa-app/static/js/main.697b4985.chunk.js"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
@@ -40,11 +36,15 @@ self.__precacheManifest = [
     "url": "/jsa-app/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/jsa-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
     "revision": "b4d13a0496a6960dfd55",
     "url": "/jsa-app/static/css/1.8a2c933c.chunk.css"
   },
   {
-    "revision": "9b3f6a8948481a2ecc4fe9fb947e9b78",
+    "revision": "02145ec459a07a5f75d19757e5c9c207",
     "url": "/jsa-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jsa-app/precache-manifest.6f0edb87c2ec151318ef0bc8e731c89a.js"
+  "/jsa-app/precache-manifest.9a0a1160063d271e87df89e8dbb85f1e.js"
 );
 
 workbox.clientsClaim();
